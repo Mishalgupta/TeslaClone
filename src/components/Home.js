d48571df6713b5e6fd7e84ed8a3620fe54002855
mishalgupta <mishalgupta2001@gmail.com>
2022-05-11 18:23:01 +0530

    version 1.1

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 import Section from './Section';
+import model_s from "../images/model-s.jpg";
+import model_y from "../images/model-y.jpg";
+import model_3 from "../images/model-3.jpg";
+import accessories from "../images/accessories.jpg";
+import solar_roof from "../images/solar-roof.jpg";
 
 const Home = () => {
     return (
@@ -8,14 +13,14 @@ const Home = () => {
             <Section
                 title="Model S"
                 description="order online for touchless delivery"
-                backgroundImg="../images/model-s.jpg"
+                backgroundImg={model_s}
                 leftBtn="Order now"
                 rightBtn="Inventory"
             />
             <Section
                 title="Model Y"
                 description="order online for touchless delivery"
-                backgroundImg="../images/model-y.jpg"
+                backgroundImg={model_y}
                 leftBtn="Order now"
                 rightBtn="Inventory"
 
@@ -23,21 +28,21 @@ const Home = () => {
             <Section
                 title="Model 3"
                 description="order online for touchless delivery"
-                backgroundImg="../images/model-3.jpg"
+                backgroundImg={model_3}
                 leftBtn="Order now"
                 rightBtn="Inventory"
             />
             <Section
                 title="Accessories"
                 description="order online for touchless delivery"
-                backgroundImg="../images/accessories.jpg"
+                backgroundImg={accessories}
                 leftBtn="Order now"
                 rightBtn="Inventory"
             />
             <Section
                 title="New solar roofs"
                 description="order online for touchless delivery"
-                backgroundImg="../images/solar-roof.jpg"
+                backgroundImg={solar_roof}
                 leftBtn="Order now"
                 rightBtn="Inventory"
             />
